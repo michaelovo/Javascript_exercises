5bdb99cf90541e355ef836e9da05a371655a4285
michael <emikeovo@gmail.com>
2020-01-10 20:01:43 +0100

    Added array and template literals to level one basics

---
+++ 02basics/marvel.js
@@ -0,0 +1,11 @@
+// using arrays([]) and Template literal(``)
+const superHeroes =['Jay JAy','Papilo','Sunday oliseh','Framk lampard']
+
+//  console.log(superHeroes);
+//  console.log(superHeroes[1]);
+console.log(superHeroes[superHeroes.length-1]); //last item on the array
+
+console.log(`we have ${superHeroes.length} heroes`);
+ 
+ 
+ 
