3a65bb424ce88a2ecc9ff7961b616ccfb281ede8
michael <emikeovo@gmail.com>
2020-01-14 19:16:34 +0100

    Added default params

--- 01intermediate/01function.js
+++ 01intermediate/01function.js
@@ -1,13 +1,13 @@
 // return strings with single parameters
 // let sayHello =function(name){
-    //     console.log(`Welcome User ${name} to Javascript functions world`);
+//         console.log(`Welcome User ${name} to Javascript functions world`);
         
-    // }
-    //sayHello('MICHAEL');
+//     }
+//     sayHello('MICHAEL');
 
 // return strings with multiple parameters
     let fullName = function(fname, lname){
-        console.log(`Welcome! ${lname} ${fname}`);
+        console.log(`Welcome! ${lname} ${fname}`); 
     }
     fullName('Micheal', 'Emmanuel');
 
@@ -16,4 +16,17 @@
         let sum = (num1 + num2);
         return sum;
     }
-    console.log(adder(5,8));
+    //console.log("Sume : "adder(5,8));
+
+    let mul = function(numA, numB){
+        return numA * numB;
+    }
+    console.log("Product : " + mul(5,8));
+
+    //setting default parameters
+    let getUser = function(name ="User", courseCount=0){
+        return 'Hello ' + name + "! Your course count : " +courseCount;
+    }
+    console.log(getUser('James', 9));
+
+
