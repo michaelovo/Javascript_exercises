3f5b4b8eabaffec6c0f7ac57e5dd7a435fb18c79
michael <emikeovo@gmail.com>
2020-01-15 19:57:21 +0100

    modified

--- 02basics/marvel.js
+++ 02basics/marvel.js
@@ -1,26 +1,26 @@
     // using arrays([]) and Template literal(``)
-    //const superHeroes =['Jay JAy','Papilo','Sunday oliseh','Framk lampard']
+    const superHeroes =['Jay JAy','Papilo','Sunday oliseh','Framk lampard']
 
-    //  console.log(superHeroes);
-    //  console.log(superHeroes[1]);
-    //console.log(superHeroes[superHeroes.length-1]); //last item on the array
+     console.log(superHeroes);
+     console.log(superHeroes[1]);
+    console.log(superHeroes[superHeroes.length-1]); //last item on the array
 
-    //console.log(`we have ${superHeroes.length} heroes`);
+    console.log(`we have ${superHeroes.length} heroes`);
     
     
     
     const numbers=['one','two','three','four','five','six']
 
-    // numbers[1]='Nigeria';
-    // console.log(numbers);
+    numbers[1]='Nigeria';
+    console.log(numbers);
 
 
-    // start
-    // numbers.shift();// displays items from [1]
-    // console.log(`Shifted array(): ${numbers}\n`);
+    start
+    numbers.shift();// displays items from [1]
+    console.log(`Shifted array(): ${numbers}\n`);
 
-    // numbers.unshift("james")
-    // console.log(`Unshifted array(): ${numbers}`);//insert from position[0]
+    numbers.unshift("james")
+    console.log(`Unshifted array(): ${numbers}`);//insert from position[0]
 
 
     //middle
@@ -30,6 +30,6 @@
 
 
     //end
-    // console.log("The element to be deleted is " +numbers.pop());
-    // numbers.push('seven');
-    // console.log(numbers);
+    console.log("The element to be deleted is " +numbers.pop());
+    numbers.push('seven');
+    console.log(numbers);
