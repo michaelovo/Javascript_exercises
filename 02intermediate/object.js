cbd3bf42d048ca5c997e7130e76d4917d3155779
michael <emikeovo@gmail.com>
2020-01-16 19:05:11 +0100

    Added a simple object

---
+++ 02intermediate/object.js
@@ -0,0 +1,12 @@
+// creating and accessing objects
+let textBook ={
+    title : 'Intro to programming',
+    author : 'Micheal ovo',
+    publisher : 'Macmillan',
+    year: '1895',
+    ISBN :'4529',
+    price :50
+}
+
+//console.log(textBook);
+console.log(`${textBook.title} Written by ${textBook.author} in ${textBook.year} and publish by ${textBook.publisher}`)
