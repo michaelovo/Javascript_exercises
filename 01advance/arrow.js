81e1ebf3056c9a0815da5225641e140146c8f94b
michael <emikeovo@gmail.com>
2020-01-29 08:04:31 +0100

    Updated with 'this' to access object values

--- 01advance/arrow.js
+++ 01advance/arrow.js
@@ -25,4 +25,23 @@ const todos = [
 // using arrow and filter function to return object based on condition
 const tinsDone = todos.filter((todo) => todo.isDone == false)
 
-console.log(todos.title(tinsDone));
+//console.log(todos.title(tinsDone));
+
+
+// using 'this' and function. NB: Never use the arrow function in methods and constructors
+const camera = {
+    price : 60000,
+    weight : '5kg',
+    color : 'white',
+    name : 'Cannon',
+
+    myDes : function(){
+        return `The camera price is $${this.price}`
+    }
+}
+console.log(camera.myDes());
+
+
+
+
+
